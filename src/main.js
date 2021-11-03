af86798b1e51181338415ce634d6c960b2d0e109
bcbro2021 <74782934+bcbro2021@users.noreply.github.com>
2021-11-03 17:58:48 +0000

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,9 @@
+document.getElementById("SnippetsToDo").onclick = function () {
+    location.href = "ToDoList.html";
+};
+document.getElementById("YVV").onclick = function () {
+    location.href = "YVV.html";
+};
+document.getElementById("CF").onclick = function () {
+    location.href = "ColorFlip.html";
+};
